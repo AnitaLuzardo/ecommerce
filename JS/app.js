5fbcf1005e0ed79d95c1662e35a7032323863263
unknown <anitaebratt@gmail.com>
2021-08-17 21:12:24 -0300

    Crea carousel banner

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,33 @@
 let menu = document.querySelector('#menu');
 let bxMenu = document.querySelector('#bx-menu');
+let slider = document.querySelector('.slider-contenedor');
+let sliderIndividual = document.querySelectorAll('.contenido-slider');
+let contador = 1;
+let width = sliderIndividual[0].clientWidth
+let intervalo = 5000;
 
 bxMenu.addEventListener('click',  () => {
   menu.classList.toggle('menu-on');
 })
+
+window.addEventListener("resize", () => {
+  width = sliderIndividual[0].clientWidth;
+})
+
+setInterval(() => {
+  slides();
+},intervalo)
+
+function slides () {
+  slider.style.transform = "translate("+(-width*contador)+"px)";
+  slider.style.transition = "transform .9s"
+  contador++;
+
+  if(contador == sliderIndividual.length){
+    setTimeout(() => {
+      slider.style.transform = "translate(0px)";
+      slider.style.transition = "transform 0s";
+      contador = 1;
+    },1500)
+  }
+}
