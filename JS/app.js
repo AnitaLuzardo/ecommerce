6dc62b86d37622cf05a0a2039330a51d2d5b95ef
unknown <anitaebratt@gmail.com>
2021-08-23 20:12:07 -0300

    Continua creación del banner

--- JS/app.js
+++ JS/app.js
@@ -1,33 +1,50 @@
 let menu = document.querySelector('#menu');
 let bxMenu = document.querySelector('#bx-menu');
-let slider = document.querySelector('.slider-container');
-let sliderIndividual = document.querySelectorAll('.contenido-slide');
-let contador = 1;
-let width = sliderIndividual[0].clientWidth
-let intervalo = 5000;
+
+const slider = document.querySelector('#slider-container');
+let sliderSection = document.querySelectorAll('.contenido-slide');
+let sliderSectionLast =  sliderSection[sliderSection.length -1]; 
+
+const btnLeft = document.querySelector('#btnleft');
+const btnRigth = document.querySelector('#btnright');
 
 bxMenu.addEventListener('click',  () => {
   menu.classList.toggle('menu-on');
 })
 
-window.addEventListener("resize", () => {
-  width = sliderIndividual[0].clientWidth;
-})
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function next (){
+  let sliderSectionFirst = document.querySelectorAll('.contenido-slide')[0];
+  slider.style.marginLeft = '-200%';
+  slider.style.transition = 'all 1s';
+  setTimeout(() => {
+    slider.style.transition = 'none';
+    slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+    slider.style.marginLeft = '-100%';
+  }, 1000);
+}
+
+function prev (){
+  let sliderSection = document.querySelectorAll('.contenido-slide');
+  let sliderSectionLast =  sliderSection[sliderSection.length -1]; 
+  slider.style.marginLeft = '0';
+  slider.style.transition = 'all 1s';
+  setTimeout(() => {
+    slider.style.transition = 'none';
+    slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+    slider.style.marginLeft = '-100%';
+  }, 1000);
+}
+
+btnRigth.addEventListener('click', function (){
+  next()
+});
+
+btnLeft.addEventListener('click', function (){
+  prev()
+});
 
 setInterval(() => {
-  slides();
-},intervalo)
-
-function slides () {
-  slider.style.transform = "translate("+(-width*contador)+"px)";
-  slider.style.transition = "transform .9s"
-  contador++;
-
-  if(contador == sliderIndividual.length){
-    setTimeout(() => {
-      slider.style.transform = "translate(0px)";
-      slider.style.transition = "transform 0s";
-      contador = 1;
-    },1500)
-  }
-}
+  next();
+}, 6000);
