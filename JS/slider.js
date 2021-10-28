ff3e62900094074e3e3bc8ac860c83aca8c70450
unknown <anitaebratt@gmail.com>
2021-10-27 22:20:18 -0300

    Crea pantalla de detalle del producto

---
+++ JS/slider.js
@@ -0,0 +1,95 @@
+//========= Comienzo del Primer Slider
+const slider = document.querySelector('#slider-container');
+let sliderSection = document.querySelectorAll('.contenido-slide');
+let sliderSectionLast =  sliderSection[sliderSection.length -1]; 
+
+const btnLeft = document.querySelector('#btnleft');
+const btnRigth = document.querySelector('#btnright');
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function next (){
+  let sliderSectionFirst = document.querySelectorAll('.contenido-slide')[0];
+  slider.style.marginLeft = '-200%';
+  slider.style.transition = 'all 1s';
+  setTimeout(() => {
+    slider.style.transition = 'none';
+    slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+    slider.style.marginLeft = '-100%';
+  }, 1000);
+}
+
+function prev (){
+  let sliderSection = document.querySelectorAll('.contenido-slide');
+  let sliderSectionLast =  sliderSection[sliderSection.length -1]; 
+  slider.style.marginLeft = '0';
+  slider.style.transition = 'all 1s';
+  setTimeout(() => {
+    slider.style.transition = 'none';
+    slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+    slider.style.marginLeft = '-100%';
+  }, 1000);
+}
+
+btnRigth.addEventListener('click', function (){
+  next()
+});
+
+btnLeft.addEventListener('click', function (){
+  prev()
+});
+
+setInterval(() => {
+  next();
+}, 6000);
+//======= Finaliza el primer Slider
+
+//======= Inicio del Segundo Slider
+const contentSlider = document.querySelector('#content-respF');
+let sliderFooter = document.querySelectorAll('.service-respFooter');
+let sliderFooterLast =  sliderFooter[sliderFooter.length -1];
+
+let btnLeftResp = document.querySelector('#btnleftResp');
+let btnRigthResp = document.querySelector('#btnrightResp');
+
+contentSlider.insertAdjacentElement('afterbegin', sliderFooterLast);
+
+function nextResp() {
+  let sliderFooterfirst = document.querySelectorAll('.service-respFooter')[0];
+  contentSlider.style.marginLeft = '-200%';
+  contentSlider.style.transition = 'all 1s';
+  setTimeout(function(){
+    contentSlider.style.transition = 'none';
+    contentSlider.insertAdjacentElement('beforeend', sliderFooterfirst);
+    contentSlider.style.marginLeft = '-100%';
+  }, 1000);
+}
+
+btnRigthResp.addEventListener('click', function(){
+  nextResp();
+})
+
+function prevResp() {
+  let sliderFooter = document.querySelectorAll('.service-respFooter');
+  let sliderFooterLast =  sliderFooter[sliderFooter.length -1];
+  contentSlider.style.marginLeft = '0';
+  contentSlider.style.transition = 'all 1s';
+  setTimeout(function(){
+    contentSlider.style.transition = 'none';
+    contentSlider.insertAdjacentElement('afterbegin', sliderFooterLast);
+    contentSlider.style.marginLeft = '-100%';
+  }, 1000);
+}
+
+btnRigthResp.addEventListener('click', function(){
+  nextResp();
+})
+
+btnLeftResp.addEventListener('click', function(){
+  prevResp();
+})
+
+setInterval(() => {
+  nextResp();
+}, 6000);   
+//========Finaliza el segundo Slider
