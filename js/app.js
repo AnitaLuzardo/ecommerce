5d3492077f3091071798cab08f1890f8df207a20
Ana Luzardo <anitaebratt@gmail.com>
2021-11-17 21:24:51 -0300

    Crea slider de productos

--- js/app.js
+++ js/app.js
@@ -4,3 +4,9 @@ let bxMenu = document.querySelector('#bx-menu');
 bxMenu.addEventListener('click',  () => {
   menu.classList.toggle('menu-on');
 })
+
+//Slider descripción de producto
+function myfuntion(smallImag){
+  var fullImag= document.getElementById("imageBox");
+  fullImag.src= smallImag.src;
+}
